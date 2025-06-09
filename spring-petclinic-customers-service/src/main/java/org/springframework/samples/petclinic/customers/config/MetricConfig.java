de27bb1fec32b9dbd03cd4ba30e476be47db0cf2
MyTruong28022004 <truongthitumy28022004@gmail.com>
2025-06-10 00:03:59 +0700

    Update MetricConfig.java

--- spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/config/MetricConfig.java
+++ spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/config/MetricConfig.java
@@ -1,22 +1,31 @@
-package org.springframework.samples.petclinic.customers.config;
-
-import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.Meter;
 import io.micrometer.core.instrument.MeterRegistry;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MetricConfig {
 
-  @Bean
-  MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
-      return registry -> registry.config().commonTags("application", "petclinic");
-  }
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config()
+            .commonTags("application", "petclinic")
+            .meterFilter(addTraceIdTag());
+    }
 
-  @Bean
-  TimedAspect timedAspect(MeterRegistry registry) {
-    return new TimedAspect(registry);
-  }
+    private MeterFilter addTraceIdTag() {
+        return MeterFilter.commonTags(
+            "trace_id", () -> {
+                SpanContext context = Span.current().getSpanContext();
+                return context.isValid() ? context.getTraceId() : "unknown";
+            });
+    }
 
+    @Bean
+    TimedAspect timedAspect(MeterRegistry registry) {
+        return new TimedAspect(registry);
+    }
 }
