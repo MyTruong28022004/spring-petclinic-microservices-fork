5f34c7dc815265491c8bb1f0203fc15c94c05e31
MyTruong28022004 <truongthitumy28022004@gmail.com>
2025-06-10 09:31:00 +0700

    Update MetricConfig.java

--- spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/config/MetricConfig.java
+++ spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/config/MetricConfig.java
@@ -2,10 +2,6 @@ package org.springframework.samples.petclinic.customers.config;
 
 import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Tag;
-import io.micrometer.core.instrument.Tags;
-import io.micrometer.core.instrument.config.MeterFilter;
-import org.slf4j.MDC;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,21 +9,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MetricConfig {
 
-    @Bean
-    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
-        return registry -> registry.config().commonTags("application", "petclinic");
-    }
+  @Bean
+  MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+    return registry -> registry.config().commonTags("application", "petclinic");
+  }
 
-    @Bean
-    public MeterFilter traceIdMeterFilter() {
-        return MeterFilter.commonTags(() -> {
-            String traceId = MDC.get("traceId");
-            return (Iterable<Tag>) Tags.of("traceId", traceId != null ? traceId : "unknown");
-        });
-    }
+  @Bean
+  TimedAspect timedAspect(MeterRegistry registry) {
+    return new TimedAspect(registry);
+  }
 
-    @Bean
-    TimedAspect timedAspect(MeterRegistry registry) {
-        return new TimedAspect(registry);
-    }
 }
