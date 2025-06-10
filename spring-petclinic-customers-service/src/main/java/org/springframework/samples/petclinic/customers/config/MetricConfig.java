1967dff1ef93296c2292c185e39495b2ebf4062e
MyTruong28022004 <truongthitumy28022004@gmail.com>
2025-06-10 09:10:54 +0700

    Update MetricConfig.java

--- spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/config/MetricConfig.java
+++ spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/config/MetricConfig.java
@@ -2,6 +2,7 @@ package org.springframework.samples.petclinic.customers.config;
 
 import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.config.MeterFilter;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
